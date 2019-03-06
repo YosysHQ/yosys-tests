9e6ba6d43b4beda9fbcce7a99884aa73893de978
SergeyDegtyar <sndegtyar@gmail.com>
2019-03-06 08:52:27 +0200

    Add tests for issues 449-527

--- regression/run.sh
+++ regression/run.sh
@@ -35,6 +35,38 @@ if [ "$1" = "issue_00089" ] ||\
 		fi
 	fi
 
+# cases where some object names are/aren't expected in output file (tee -o result.log in the test script)
+elif [ "$1" = "issue_00502" ] ||\
+     [ "$1" = "issue_00524" ] ||\
+     [ "$1" = "issue_00527" ]; then
+
+	expected_string=""
+	expected="1"
+	if [ "$1" = "issue_00502" ]; then
+		expected_string="\\SUM/N10"
+	elif [ "$1" = "issue_00524" ]; then
+		expected_string="GP_INV"
+	elif [ "$1" = "issue_00527" ]; then
+		expected_string="DFFSR"
+		expected="0"
+	fi
+
+	yosys -ql yosys.log ../../scripts/$2.ys;
+	if grep "$expected_string" result.log; then
+		if [ $expected = "1" ]; then
+			echo pass > ${1}_${2}.status
+		else
+			echo fail > ${1}_${2}.status
+		fi
+	else
+		if [ $expected = "1" ]; then
+			echo fail > ${1}_${2}.status
+		else
+			echo pass > ${1}_${2}.status
+		fi
+	fi
+
+
 # cases with simulation checks
 else
 
@@ -44,12 +76,13 @@ else
 		iverilog_adds="../../../../../techlibs/xilinx/brams_bb.v"
 	elif [ "$1" = "issue_00160" ] ||\
 		 [ "$1" = "issue_00182" ] ||\
-		 [ "$1" = "issue_00183" ]; then
+		 [ "$1" = "issue_00183" ] ||\
+		 [ "$1" = "issue_00481" ]; then
 		iverilog_adds="../../../../../techlibs/ice40/cells_sim.v"
 	fi
 
 	yosys -ql yosys.log ../../scripts/$2.ys
-	iverilog -o testbench  ../testbench.v synth.v ../../common.v ../../../../../techlibs/common/simcells.v $iverilog_adds
+	iverilog -o testbench  ../testbench.v synth.v ../../common.v ../../../../../techlibs/common/simcells.v ../../../../../techlibs/common/simlib.v $iverilog_adds
 	if ! vvp -N testbench > testbench.log 2>&1; then
 		grep 'ERROR' testbench.log
 		echo fail > ${1}_${2}.status
