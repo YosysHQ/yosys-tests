c02cd167b4a155d24a155f52026a2c8a0bb2279d
SergeyDegtyar <sndegtyar@gmail.com>
2019-03-19 18:15:51 +0200

    Add new tests to 'simple' and 'misc' groups
    Testing problems:
    
    1. "sat -tempinduct_def":
    +#ERROR: Assert `!undef_mode || model_undef' failed in
    ./kernel/satgen.h:90.
    
    2. "share -force":
    +#ERROR: Abort in passes/opt/share.cc:724.

--- misc/tee/top.v
+++ misc/tee/top.v
@@ -7,13 +7,7 @@ module top
  output reg A,
  output cout
  );
- parameter X = 1;
  wire o;
-
- initial A = 0;
- initial cout = 0;
-
-
 
 `ifndef BUG
 always @(posedge cin)
@@ -21,8 +15,7 @@ always @(posedge cin)
 
 assign cout =  cin? y : x;
 
-middle u_mid (.x(x),.o(o));
-u_rtl inst_u_rtl (.x(x),.o(o));
+middle u_mid (x,y,o);
 `else
 assign {cout,A} =  cin - y * x;
 `endif
@@ -30,16 +23,6 @@ assign {cout,A} =  cin - y * x;
 endmodule
 
 module middle
-(
-	input x,
-	input y,
-	output o
-);
-
-assign o = x + y;
-endmodule
-
-module u_rtl
 (
 	input x,
 	input y,
