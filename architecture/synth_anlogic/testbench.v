f9f4d7b7c207c2e92dcca5c68f44e921dee682a9
SergeyDegtyar <sndegtyar@gmail.com>
2019-05-02 15:58:28 +0300

    Fix failed tests for dffe cell

--- architecture/synth_anlogic/testbench.v
+++ architecture/synth_anlogic/testbench.v
@@ -11,10 +11,10 @@ module testbench;
             #5 clk = 0;
         end
 
-        $display("OKAY");    
+        $display("OKAY");
     end
-   
-    
+
+
     reg [2:0] dinA = 0;
     wire doutB,doutB1,doutB2,doutB3,doutB4;
 	reg dff,ndff,adff,adffn,dffe = 0;
@@ -30,12 +30,12 @@ module testbench;
         .b3 (doutB3 ),
         .b4 (doutB4 )
     );
-    
+
     always @(posedge clk) begin
     #3;
     dinA <= dinA + 1;
     end
-	
+
 	always @( posedge clk, posedge dinA[1], posedge dinA[2] )
 		if ( dinA[2] )
 			dff <= 1'b0;
@@ -43,7 +43,7 @@ module testbench;
 			dff <= 1'b1;
 		else
             dff <= dinA[0];
-    
+
     always @( negedge clk, negedge dinA[1], negedge dinA[2] )
 		if ( !dinA[2] )
 			ndff <= 1'b0;
@@ -51,27 +51,27 @@ module testbench;
 			ndff <= 1'b1;
 		else
             ndff <= dinA[0];
-            
+
     always @( posedge clk, posedge dinA[2] )
 		if ( dinA[2] )
 			adff <= 1'b0;
 		else
             adff <= dinA[0];
-            
+
     always @( posedge clk, negedge dinA[2] )
 		if ( !dinA[2] )
 			adffn <= 1'b0;
 		else
             adffn <= dinA[0];
-            
-    always @( posedge clk, posedge dinA[2] )
+
+    always @( posedge clk)
 		if ( dinA[2] )
             dffe <= dinA[0];
-            
+
 	assert_dff dff_test(.clk(clk), .test(doutB), .pat(dff));
     assert_dff ndff_test(.clk(clk), .test(doutB1), .pat(ndff));
-    assert_dff adff_test(.clk(clk), .test(doutB2), .pat(adff));    
+    assert_dff adff_test(.clk(clk), .test(doutB2), .pat(adff));
     assert_dff adffn_test(.clk(clk), .test(doutB3), .pat(adffn));
     assert_dff dffe_test(.clk(clk), .test(doutB4), .pat(dffe));
-    
+
 endmodule
