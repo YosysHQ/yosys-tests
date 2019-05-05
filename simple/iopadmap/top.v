984a011dd910b676dfe3ccb6bd4d910795143bab
Miodrag Milanovic <mmicko@gmail.com>
2019-05-05 09:46:55 +0200

    proper falsify

--- simple/iopadmap/top.v
+++ simple/iopadmap/top.v
@@ -17,10 +17,10 @@ module tristate (en, i, io, o);
     assign o = (en)? io : 2'bZZ;
 `else
 	always @(en or i)
-		io_buf[0] <= (!en)? ~i : 1'bZ;
+		io_buf[0] <= (en)? ~i : 1'bZ;
 
     always @(en or i)
-		io_buf[1] <= (!i)? ~en : 1'bZ;
+		io_buf[1] <= (i)? ~en : 1'bZ;
 
     assign o = (en)? ~io : 2'bZZ;
 `endif
