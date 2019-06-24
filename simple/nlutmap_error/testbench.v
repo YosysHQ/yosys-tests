e92440623f9c6c9be4c9c295657449ae3e146598
SergeyDegtyar <sndegtyar@gmail.com>
2019-06-24 13:23:50 +0300

    Add tests for error messages to 'simple' group

--- simple/nlutmap_error/testbench.v
+++ simple/nlutmap_error/testbench.v
@@ -5,8 +5,8 @@ module testbench;
 	wire patt_carry_out,carryout;
 
     initial begin
-         $dumpfile("testbench.vcd");
-         $dumpvars(0, testbench);
+        // $dumpfile("testbench.vcd");
+        // $dumpvars(0, testbench);
 
         #5 in = 0;
         repeat (10000) begin
@@ -23,11 +23,10 @@ module testbench;
 	.A(out),
 	.cout(carryout)
 	);
+	
+	assign {patt_carry_out,patt_out} =  in[2] + in[1] + in[0];
 
-	assign {patt_carry_out,patt_out} =  in[2] % in[1] / in[0];
-
-	assert_Z out_test(in[0], out);
-	assert_Z carry_test(in[0], carryout);
+	assert_comb out_test(.A(patt_out), .B(out));
+	assert_comb carry_test(.A(patt_carry_out), .B(carryout));
 
 endmodule
-
--- simple/nlutmap_error/top.v
+++ simple/nlutmap_error/top.v
@@ -7,11 +7,16 @@ module top
  output A,
  output cout
  );
-
+wire  p,r,s;
+	xor (p,x,y);
 `ifndef BUG
-assign {cout,A} =  cin % y / x;
+	xor (A,p,cin);
 `else
-assign {cout,A} =  2'bZZ;
+	and (A,p,cin);
 `endif
+ 
+	and(r,p,cin);
+	and(s,x,y);
+	or(cout,r,s);
 
 endmodule
