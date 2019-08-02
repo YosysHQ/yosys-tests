24a1fc5aba0662a9412871900cc5b87308453522
David Shah <dave@ds0.me>
2019-08-02 13:51:20 +0100

    Add -Wno-unoptflat
    Signed-off-by: David Shah <dave@ds0.me>

--- architecture/synth_xilinx_dsp/run-test-verilator.sh
+++ architecture/synth_xilinx_dsp/run-test-verilator.sh
@@ -5,6 +5,6 @@ cp ~/yosys/yosys-bench/verilog/benchmarks_small/mul/generate.py generate_mul.py
 PYTHONPATH=".:$PYTHONPATH" python3 generate_mul.py
 PYTHONPATH=".:$PYTHONPATH" python3 ../create_miter.py
 ${MAKE:-make} -f ../../../common/syn.mk YOSYS=../../../../yosys SYN_COMMAND=synth_xilinx mul*.v
-${VERILATOR:-verilator} -Wno-lint -Wno-combdly -O3 -CFLAGS -O3 -o ../mul_tb -cc --top-module top --exe *.v ../../../../techlibs/xilinx/cells_sim.v ../../../common/verilator/miter_testbench.cc
+${VERILATOR:-verilator} -Wno-unoptflat -Wno-lint -Wno-combdly -CFLAGS "-O3 -march=native" -o ../mul_tb -cc --top-module top --exe *.v ../../../../techlibs/xilinx/cells_sim.v ../../../common/verilator/miter_testbench.cc
 ${MAKE:-make} -C obj_dir -f Vtop.mk
 ./mul_tb
