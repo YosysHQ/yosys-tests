b163e90bd02f2a185b23d8408ba16cfa819d5598
David Shah <dave@ds0.me>
2019-08-13 10:32:48 +0100

    Update verilator test script
    Signed-off-by: David Shah <dave@ds0.me>

--- architecture/synth_xilinx_dsp/run-test-verilator.sh
+++ architecture/synth_xilinx_dsp/run-test-verilator.sh
@@ -7,8 +7,8 @@ PYTHONPATH=".:$PYTHONPATH" python3 ../split_groups.py
 for d in miter_group_*/; do
 	pushd $d
 	PYTHONPATH=".:$PYTHONPATH" python3 ../../create_miter.py
-	${MAKE:-make} -f ../../../../common/syn.mk YOSYS=../../../../../yosys SYN_COMMAND="synth_xilinx; clean" mul*.v
-	${VERILATOR:-verilator} -Wno-unoptflat -Wno-lint -Wno-combdly -CFLAGS "-O3" -o ../mul_tb -cc --top-module top --exe *.v ../../../../../techlibs/xilinx/cells_sim.v ../../../../common/verilator/miter_testbench.cc
+	${MAKE:-make} -f ../../../../common/syn.mk YOSYS=../../../../../yosys SYN_COMMAND="synth_xilinx -run :coarse; clean" mul*.v
+	${VERILATOR:-verilator} -Wno-initialdly -Wno-unoptflat -Wno-lint -Wno-combdly -CFLAGS "-O3" -o ../mul_tb -cc --top-module top --exe *.v ../../../../../techlibs/xilinx/cells_sim.v ../../../../common/verilator/miter_testbench.cc
 	${MAKE:-make} -C obj_dir -f Vtop.mk
 	./mul_tb -N 2500000
 	popd
