9af3cae7da4f663a2fff91424cf35a90f8b5874a
SergeyDegtyar <sndegtyar@gmail.com>
2019-11-12 16:47:19 +0200

    Minor fixes

--- backends_reviewed/write_xaiger/top_cant_find_top.v
+++ backends_reviewed/write_xaiger/top_cant_find_top.v
@@ -21,7 +21,7 @@ module top
     end
  end
 
-`ifndef BUG
+
 always @(posedge x) begin
     if ($initstate)
         A <= 0;
@@ -36,9 +36,6 @@ always @(negedge x) begin
     assert(ASSERT);
     assert(s_eventually ASSERT);
 end
-`else
-assign {cout,A} =  cin - y * x;
-`endif
 
 endmodule
 
