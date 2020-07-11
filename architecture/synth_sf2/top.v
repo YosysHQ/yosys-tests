6fd3dbda9746d181e0bb88496698fc0e426b2415
Miodrag Milanovic <mmicko@gmail.com>
2020-07-11 11:08:31 +0200

    Fix for SF2

--- architecture/synth_sf2/top.v
+++ architecture/synth_sf2/top.v
@@ -1,17 +1,11 @@
 module dff
     ( input d, clk, output reg q );
-    initial begin
-      q = 0;
-    end
 	always @( posedge clk )
             q <= d;
 endmodule
 
 module dffe
     ( input d, clk, en, output reg q );
-    initial begin
-      q = 0;
-    end
 	always @( posedge clk)
 		if ( en )
 			q <= d;
@@ -19,9 +13,6 @@ endmodule
 
 module adff
     ( input d, clk, clr, output reg q );
-    initial begin
-      q = 0;
-    end
 	always @( posedge clk, posedge clr )
 		if ( clr )
 			q <= 1'b0;
--- architecture/synth_sf2_lcov/top.v
+++ architecture/synth_sf2_lcov/top.v
@@ -1,8 +1,5 @@
 module dff
     ( input d, clk, output reg q );
-    initial begin
-      q = 0;
-    end
 	always @( posedge clk )
             q <= d;
 endmodule
